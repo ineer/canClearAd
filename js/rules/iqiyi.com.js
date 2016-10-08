611b8c339a4aa549cf606247b8fb7019174d9c49
Ye Feng <1264669423@qq.com>
2016-10-07 21:31:31 -0500

    Add files via upload

---
+++ js/rules/iqiyi.com.js
@@ -0,0 +1,20 @@
+// 移除规则
+// 符合规则的元素直接移除
+var removeRule = [
+		
+	];
+// 替换规则
+// 符合规则的元素用透明元素替换
+var replaceRule = [
+		// ['', 0]
+	];
+jumpAds();
+
+function jumpAds() {
+    var temp_video = document.querySelector('video');
+    console.log(temp_video.src);
+    if (temp_video.src.indexOf('http://data.video.qiyi.com/videos/other/') > -1) {
+        temp_video.playbackRate = 10;
+        setTimeout(jumpAds, 2000);
+    }
+}

045fcea22f754ab1f302c986ee17a58e41ecb952
Ye Feng <1264669423@qq.com>
2016-10-08 08:02:46 +0800

    Delete result.js

--- js/result.js
+++
@@ -1,7 +0,0 @@
-/*!
- * canClearAd v0.1.0
- * Copyright 2016 Ineer
- * Licensed under MIT (https://github.com/ineer/canclearad/blob/master/LICENSE)
- */
-
-console.log('本次清除了' + adClearNum + '个广告');
